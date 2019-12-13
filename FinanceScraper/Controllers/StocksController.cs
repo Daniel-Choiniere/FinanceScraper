5ac9e3509947aaf691e7ac7beea7b278f27716c5
Daniel Choiniere <dpchoiniere@gmail.com>
2019-12-13 19:32:56 +0000

    so moved over all my code from the .net core app that I was building to the .NET app I am currently working on

---
+++ FinanceScraper/Controllers/StocksController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using FinanceScraper.Models;
+using FinanceScraper.ViewModels;
+
+namespace FinanceScraper.Controllers
+{
+    // GET: stocks/   abstract list of all stocks
+    public class StocksController : Controller
+    {
+        public ViewResult Index()
+        {
+            var stocks = GetStocks();
+
+            return View(stocks);
+        }
+
+        private IEnumerable<Stock> GetStocks()
+        {
+            return new List<Stock>
+            {
+                new Stock { id = 1, Symbol = "BTC" },
+                new Stock { id = 2, Symbol = "ETH" }
+            };
+        }
+
+
+        // GET: stocks/random
+        public ActionResult Random()
+        {
+            var stock = new Stock() { Symbol = "BTC" };
+            var users = new List<User>
+            {
+                new User { Name = "Dan" },
+                new User { Name = "Bean" },
+                new User { Name = "Sean" },
+                new User { Name = "Al" },
+                new User { Name = "Christian" },
+                new User { Name = "Joe" }
+            };
+
+            var viewModel = new RandomStockViewModel
+            {
+                Stock = stock,
+                Users = users
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ FinanceScraper/ViewModels/RandomStockViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FinanceScraper.Models;
+
+namespace FinanceScraper.ViewModels
+{
+    public class RandomStockViewModel
+    {
+        public Stock Stock { get; set; }
+        public List<User> Users { get; set; }
+    }
+}

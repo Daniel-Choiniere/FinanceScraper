df95cb6c8dbe3d54ea149fc717362399018aa979
Daniel Choiniere <dpchoiniere@gmail.com>
2019-12-13 20:09:07 +0000

    have front end up and running

--- FinanceScraper/App_Start/BundleConfig.cs
+++ FinanceScraper/App_Start/BundleConfig.cs
@@ -20,10 +20,11 @@ namespace FinanceScraper
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-cyborg.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }

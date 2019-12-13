2487f077298602b0a8dc80dfc3c54161c43553d4
Daniel Choiniere <dpchoiniere@gmail.com>
2019-12-13 21:24:44 +0000

    using Entity Framework scaffoled a database table for our users

---
+++ FinanceScraper/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace FinanceScraper.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FinanceScraper.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FinanceScraper.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
